3534521e1d930a4974043fe6d160fa9d5346482f
ztiago3 <ztiago@gmail.com>
2019-09-22 18:45:13 -0300

    teste

---
+++ README.md
@@ -0,0 +1,1 @@
+# CursoUdemyReact
---
+++ exercicio-react/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const elemento = document.getElementById('root')
+ReactDOM.render(
+    <h1>Olá React2</h1>, elemento
+)
