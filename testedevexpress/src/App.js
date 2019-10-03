de851a7dc567e72b7c0bcb7b8b45593d228a9b34
Tiago Zaniquelli <15D1DF@NOTE027>
2019-10-03 15:15:17 -0300

    testedevexpress

---
+++ testedevexpress/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from './logo.svg';
+import TextBox from 'devextreme-react/text-box';
+import DataGrid, { Column, Export, Editing, Grouping, GroupPanel, Pager, Paging, ColumnChooser } from 'devextreme-react/data-grid';
+import './App.css';
+
+import service from './data.js';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.dataSource = service.getOrders();
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>teste</h1>
+
+        <DataGrid
+          id={'gridContainer'}
+          dataSource={this.dataSource}
+          columnHidingEnabled={true}
+          keyExpr={'ID'}
+          showBorders={true}>
+          <Export enabled={true} fileName={'Orders'} />
+          <Editing allowAdding={true} allowUpdating={true} mode={'batch'} />
+          <Grouping contextMenuEnabled={true} expandMode={'rowClick'} />
+          <GroupPanel visible={true} emptyPanelText={'Use the context menu of header columns to group data'} />
+          <Pager
+            allowedPageSizes={[5, 8, 15, 30]}
+            showInfo={true}
+            showNavigationButtons={true}
+            showPageSizeSelector={true}
+            visible={true} />
+          <Paging pageSize={8} />
+          <ColumnChooser enabled={true} mode={'select'} />
+          <Column allowGrouping={false} dataField={'OrderNumber'} width={130} caption={'Invoice Number'} />
+          <Column dataField={'CustomerStoreCity'} caption={'City'} />
+          <Column dataField={'CustomerStoreState'} caption={'State'} />
+          <Column dataField={'Employee'} />
+          <Column dataField={'OrderDate'} dataType={'date'} />
+          <Column dataField={'SaleAmount'} format={'currency'} />
+        </DataGrid>
+      </div>
+    );
+  }
+}
+
+export default App;
