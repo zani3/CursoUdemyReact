780bbbdfc92dba88843ece572f79bfef70621431
ztiago3 <ztiago@gmail.com>
2019-10-09 19:27:22 -0300

    teste tiago

---
+++ my-money-app/frontend/src/common/template/menuItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default props => (
+    <li> 
+        {/* <Link to={props.path}>
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
+        </Link> */}
+
+        <a href={props.path}>
+            <i className={`fa fa-${props.icon}`}></i>{props.label}
+        </a>
+    </li>
+)
---
+++ my-money-app/frontend/src/main/app.jsx
@@ -0,0 +1,23 @@
+import '../common/template/dependencies'
+import React from 'react'
+
+import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
+// import Messages from '../common/msg/messages'
+
+export default props => (
+    <div className='wrapper'>
+        <Header />
+        <SideBar />
+        <div className='content-wrapper'> 
+            <h1>conteudo</h1>
+        </div>
+        {/* <div className='content-wrapper'> 
+            {props.children}
+        </div> */}
+
+        <Footer />
+        {/* <Messages /> */}
+    </div>
+)
